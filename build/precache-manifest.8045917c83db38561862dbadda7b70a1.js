ee048ad47f70853bb9aeaf9a26ff8e341e2963c4
BENRI NGULLY <39947379+blank50@users.noreply.github.com>
2022-06-02 13:01:57 +0800

    Add files via upload

---
+++ build/precache-manifest.8045917c83db38561862dbadda7b70a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b987e69bc68d3d3696b4043e0084fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db600757f0791b28234",
+    "url": "/static/css/main.b3f6fc78.chunk.css"
+  },
+  {
+    "revision": "b33d82c799da2d8a968e",
+    "url": "/static/js/2.efe2f13e.chunk.js"
+  },
+  {
+    "revision": "0db600757f0791b28234",
+    "url": "/static/js/main.15623024.chunk.js"
+  },
+  {
+    "revision": "91da2b43fd82e3ee33f4",
+    "url": "/static/js/runtime-main.23c33b97.js"
+  },
+  {
+    "revision": "7f8e894f71a50a1b4fa24260283eaa2f",
+    "url": "/static/media/640-2.7f8e894f.jpg"
+  },
+  {
+    "revision": "fc414eafcdd51d744aa9763e93e22edd",
+    "url": "/static/media/6403.fc414eaf.jpg"
+  },
+  {
+    "revision": "08b8f2f738a11a16b98bab3073f98010",
+    "url": "/static/media/bosh.08b8f2f7.jpg"
+  },
+  {
+    "revision": "73c1dd7b3e5e263c32b3b1a9c5f642f9",
+    "url": "/static/media/comfort.73c1dd7b.jpg"
+  },
+  {
+    "revision": "fc18a5f4ca1f54a3340d250a8cbacb38",
+    "url": "/static/media/design.fc18a5f4.jpg"
+  },
+  {
+    "revision": "6f0a1c3d16ca5359e4956c7750299fa2",
+    "url": "/static/media/handling.6f0a1c3d.jpg"
+  },
+  {
+    "revision": "35d0752831e390daf89088f58ca61083",
+    "url": "/static/media/headlight.35d07528.jpg"
+  },
+  {
+    "revision": "5941b9172c003d857d217ad2960185b3",
+    "url": "/static/media/safety.5941b917.jpg"
+  },
+  {
+    "revision": "01586e4f9d296b9d76585375d42c67e7",
+    "url": "/static/media/spec.01586e4f.jpg"
+  },
+  {
+    "revision": "c8cd172cd52d0bd4109d5a82e393008d",
+    "url": "/static/media/wenzhou.c8cd172c.jpg"
+  }
+]);
